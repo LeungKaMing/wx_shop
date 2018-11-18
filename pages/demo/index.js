6afdbef32da4f6551f352e6af750fca00c371b2b
leung <510314588@qq.com>
2018-11-19 00:35:12 +0800

    feat: :wrench: 1118 完成首页基本ui

--- pages/demo/index.js
+++ pages/demo/index.js
@@ -1,34 +1,39 @@
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    logo: '../../assets/images/youshu_logo.png',
+    searchLogo: '../../assets/images/search_logo.png',
+    filterLogo: '../../assets/images/filter_logo.png',
     currentGoodIndex: 0,
     goodsList: [
       {
-        pic: '',
-        desc: 'buy',
-        comment: '0',
-        user: '辛德瑞拉',
+        pic: 'http://img3m6.ddimg.cn/13/35/25292236-2_w_3.jpg',
+        desc: '《无限》（精装版）',
+        comment: '71',
+        avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+        name: '乔纳森·希克曼',
         time: '30秒前',
-        location: '566'
+        location: '广州'
       },
       {
-        pic: '',
-        desc: 'buy',
-        comment: '0',
-        user: '辛德瑞拉',
-        time: '30秒前',
-        location: '566'
+        pic: 'http://img3m4.ddimg.cn/2/33/25265594-1_b_3.jpg',
+        desc: '《复仇者联盟3无限战争》电影档案',
+        comment: '1907',
+        avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+        name: '杰罗姆·奥彭阿',
+        time: '1天前',
+        location: '广州'
       },
       {
-        pic: '',
-        desc: 'buy',
-        comment: '0',
-        user: '辛德瑞拉',
-        time: '30秒前',
-        location: '566'
+        pic: 'http://img3m2.ddimg.cn/97/26/25304992-1_b_2.jpg',
+        desc: '灭霸崛起',
+        comment: '2321',
+        avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+        name: '四川美术出版社',
+        time: '1小时前',
+        location: '广州'
       }
     ]
   },
@@ -52,28 +57,31 @@ Page({
       currentGoodIndex: 0,
       goodsList: [
         {
-          pic: '',
-          desc: 'buy',
-          comment: '0',
-          user: '辛德瑞拉',
+          pic: 'http://img3m6.ddimg.cn/13/35/25292236-2_w_3.jpg',
+          desc: '《无限》（精装版）',
+          comment: '71',
+          avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+          name: '乔纳森·希克曼',
           time: '30秒前',
-          location: '566'
+          location: '广州'
         },
         {
-          pic: '',
-          desc: 'buy',
-          comment: '0',
-          user: '辛德瑞拉',
-          time: '30秒前',
-          location: '566'
+          pic: 'http://img3m4.ddimg.cn/2/33/25265594-1_b_3.jpg',
+          desc: '《复仇者联盟3无限战争》电影档案',
+          comment: '1907',
+          avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+          name: '杰罗姆·奥彭阿',
+          time: '1天前',
+          location: '广州'
         },
         {
-          pic: '',
-          desc: 'buy',
-          comment: '0',
-          user: '辛德瑞拉',
-          time: '30秒前',
-          location: '566'
+          pic: 'http://img3m2.ddimg.cn/97/26/25304992-1_b_2.jpg',
+          desc: '灭霸崛起',
+          comment: '2321',
+          avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+          name: '四川美术出版社',
+          time: '1小时前',
+          location: '广州'
         }
       ]
     })
@@ -84,28 +92,31 @@ Page({
       currentGoodIndex: 1,
       goodsList: [
         {
-          pic: '',
-          desc: 'borrow',
-          comment: '0',
-          user: '辛德瑞拉',
+          pic: 'http://img3m6.ddimg.cn/66/24/23826396-1_b_2.jpg',
+          desc: '超人-秘密身份',
+          comment: '71',
+          avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+          name: '乔纳森·希克曼',
           time: '30秒前',
-          location: '566'
+          location: '广州'
         },
         {
-          pic: '',
-          desc: 'borrow',
-          comment: '0',
-          user: '辛德瑞拉',
-          time: '30秒前',
-          location: '566'
+          pic: 'http://img3m4.ddimg.cn/58/25/1018813414-1_b_1.jpg',
+          desc: '天国降临',
+          comment: '1907',
+          avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+          name: '杰罗姆·奥彭阿',
+          time: '1天前',
+          location: '广州'
         },
         {
-          pic: '',
-          desc: 'borrow',
-          comment: '0',
-          user: '辛德瑞拉',
-          time: '30秒前',
-          location: '566'
+          pic: 'http://img3m8.ddimg.cn/70/32/1290749398-1_b_1.jpg',
+          desc: '无限危机',
+          comment: '2321',
+          avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+          name: '四川美术出版社',
+          time: '1小时前',
+          location: '广州'
         }
       ]
     })
@@ -116,28 +127,31 @@ Page({
       currentGoodIndex: 2,
       goodsList: [
         {
-          pic: '',
-          desc: 'give',
-          comment: '0',
-          user: '辛德瑞拉',
+          pic: 'http://img3m0.ddimg.cn/27/5/25337790-1_b_3.jpg',
+          desc: '零年：黑暗之城',
+          comment: '71',
+          avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+          name: '乔纳森·希克曼',
           time: '30秒前',
-          location: '566'
+          location: '广州'
         },
         {
-          pic: '',
-          desc: 'give',
-          comment: '0',
-          user: '辛德瑞拉',
-          time: '30秒前',
-          location: '566'
+          pic: 'http://img3m6.ddimg.cn/39/19/23909826-1_b_2.jpg',
+          desc: '灭族之灾',
+          comment: '1907',
+          avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+          name: '杰罗姆·奥彭阿',
+          time: '1天前',
+          location: '广州'
         },
         {
-          pic: '',
-          desc: 'give',
-          comment: '0',
-          user: '辛德瑞拉',
-          time: '30秒前',
-          location: '566'
+          pic: 'http://img3m9.ddimg.cn/2/17/25183919-1_b_3.jpg',
+          desc: '正义联盟',
+          comment: '2321',
+          avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
+          name: '四川美术出版社',
+          time: '1小时前',
+          location: '广州'
         }
       ]
     })
