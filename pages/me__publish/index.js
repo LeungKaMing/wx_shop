697d32573667e5b19e5d05af4fcfa8f507255665
leung <510314588@qq.com>
2018-11-21 23:41:22 +0800

    feat: :lipstick: 1121 完成“我的页面”-“我的发布”ui

--- pages/me__publish/index.js
+++ pages/me__publish/index.js
@@ -3,56 +3,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-    perList: [
-      {
-        id: 1,
-        img: '../../assets/images/send.png',
-        name: '我的发布'
-      },
-      {
-        id: 2,
-        img: '../../assets/images/download.png',
-        name: '已下架'
-      },
-      {
-        id: 3,
-        img: '../../assets/images/order.png',
-        name: '我的订单'
-      },
-      {
-        id: 4,
-        img: '../../assets/images/vip.png',
-        name: '我的会员'
-      },
-      {
-        id: 5,
-        img: '../../assets/images/wallet.png',
-        name: '我的钱包'
-      },
-      {
-        id: 6,
-        img: '../../assets/images/collect.png',
-        name: '我的收藏'
-      },
-      {
-        id: 7,
-        img: '../../assets/images/addr.png',
-        name: '我的地址'
-      },
-      {
-        id: 8,
-        img: '../../assets/images/settings.png',
-        name: '设置'
-      }
+    imgUrls: [
+      '../../assets/images/1.png',
+      '../../assets/images/2.png',
+      '../../assets/images/3.png'
     ],
-    arrowImg: '../../assets/images/goTo.png',
-    bookDetails: {
-      avatar: 'https://avatars3.githubusercontent.com/u/18412359?s=40&v=4',
-      name: '乔纳森',
-      gender: 'male',
-      tag: 'vip',
-      time: '30秒前'
-    }
+    arrowImg: '../../assets/images/goTo.png'
   },
 
   /**
